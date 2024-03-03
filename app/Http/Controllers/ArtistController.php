2b1221b542c1008e20087576f83843d027e5e20e
redouan <ridouan.sen@outlook.com>
2024-03-03 12:57:04 +0100

    Update modèle User +migration +seeders

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -116,6 +116,13 @@ class ArtistController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $artist = Artist::find($id);
+
+        if($artist) {
+            $artist->delete();
+        }
+
+        return redirect()->route('artist.index');
+
     }
 }
--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -7,6 +7,13 @@
         @if($artist)
             <h1>{{ $artist->firstname }} {{ $artist->lastname }}</h1>
             <div><a href="{{ route('artist.edit' ,$artist->id) }}" >Modifier</a></div>
+            <form method="post" action="{{ route('artist.delete', $artist->id) }}">
+                @csrf
+                <!-- <input type="hidden" name="method" value="DELETE"> -->
+                @method('DELETE')
+                <button>Supprimer</button>
+            </form>
+
             <nav><a href="{{ route('artist.index') }}">Retour à l'index</a></nav>
         @else
             <p>Aucun artiste trouvé.</p>
