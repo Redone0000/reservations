1eeca4e0cd772a358309d77d070975ebc5310e8d
redouan <ridouan.sen@outlook.com>
2024-03-06 16:26:02 +0100

    Update templates Show (added artists’ list)

--- app/Models/Show.php
+++ app/Models/Show.php
@@ -53,4 +53,11 @@ class Show extends Model
         return $this->hasMany(Representation::class);
     }
 
+        /**
+     * Get the performances (artists in a type of collaboration) for the show
+     */
+    public function artistTypes()
+    {
+        return $this->belongsToMany(ArtistType::class);
+    }
 }
